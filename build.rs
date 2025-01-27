1b1f2181c2510b87d19d5d8a03a9f053d39ad039
Liam Kinne <liam@kinne.io>
2025-01-27 16:57:51 +1000

    cleanup comments

--- build.rs
+++ build.rs
@@ -5,32 +5,34 @@ use std::{
 use tlvc_text::{load, pack};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Get out directory.
+    // get output directory
     let out = &PathBuf::from(std::env::var("OUT_DIR")?);
 
-    // Optionally inject vital product data.
+    // optionally inject vital product data
     println!("cargo:rerun-if-changed=vpd.ron");
     if let Some(path) = option_env!("WRITE_VPD") {
         let vpd_file = File::open(path)?;
         let vpd = pack(&load(vpd_file)?);
         File::create(out.join("vpd.bin"))?.write_all(&vpd)?;
     } else {
-        // write empty file to satisfy clippy.
+        // write empty file to satisfy clippy
         File::create(out.join("vpd.bin"))?.set_len(0)?;
     }
 
-    // put `memory.x` in our output directory and ensure it's on the linker
+    // put `memory.x` in the output directory and ensure it's in the linker
     // search path.
     File::create(out.join("memory.x"))?
         .write_all(include_bytes!("memory.x"))?;
     println!("cargo:rustc-link-search={}", out.display());
 
+    // inject compilation timestamp
     let date_time: DateTime<Utc> = SystemTime::now().into();
     println!(
         "cargo:rustc-env=CRATE_BUILT_AT={}",
         date_time.to_rfc3339_opts(SecondsFormat::Secs, true)
     );
 
+    // inject git commit hash
     let git_hash = String::from_utf8(
         Command::new("git")
             .args(["rev-parse", "HEAD"])
